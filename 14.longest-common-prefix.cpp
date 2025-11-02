60ea61ed28c8fdca5619c706d70366e4d8efc870
Ding Yanwen <182461378+SylviaDillion@users.noreply.github.com>
2025-11-02 13:29:01 +0800

    Q14

--- 14.longest-common-prefix.cpp
+++ 14.longest-common-prefix.cpp
@@ -5,10 +5,27 @@
  */
 
 // @lc code=start
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        
+        string result;
+        for(int i = 0; i<strs[0].size(); ++i){//表示字母
+            for(int j=0; j<strs.size(); ++j){//表示vector
+                if(j == strs.size()-1){
+                    result+=strs[0][i];
+                    break;
+                }
+                if(strs[j][i] == strs[j+1][i])
+                    continue;
+                else
+                    return result;
+            }
+        }
+        return result;
     }
 };
 // @lc code=end
